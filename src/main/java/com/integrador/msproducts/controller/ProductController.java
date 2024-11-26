0cbfc6ccbae7ea0425c1811896bd0e6a546c49e6
Agustin Vessoni <agustinvessoni149@gmail.com>
2024-11-26 07:05:20 -0600

    front url updated in CrossOrigin anotations

--- src/main/java/com/integrador/msproducts/controller/ProductController.java
+++ src/main/java/com/integrador/msproducts/controller/ProductController.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
 
 @RestController
 @CrossOrigin(origins = {
-        "https://marketplace-qp7hjybwl-vessoni149s-projects.vercel.app/",
+        "https://marketplace-vessoni149s-projects.vercel.app/",
         }, allowCredentials = "true")
 public class ProductController {
     @Autowired
